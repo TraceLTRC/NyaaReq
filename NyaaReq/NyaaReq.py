234cb0fbb4804010bbba5714b4dec2cfdffeaad8
TraceLosu <kivlanbahmidt@gmail.com>
2020-10-28 18:01:32 +0700

    Added CLI function

--- NyaaReq/NyaaReq.py
+++ NyaaReq/NyaaReq.py
@@ -1,13 +1,22 @@
 from lxml import html
-import requests
+from pprint import pprint as print
+from os import path
+import requests, json, argparse
 
 
 class NyaaReq():
     def __init__(self):
         self.site = 'https://nyaa.si'
         self.siteQuery = self.site + '/?f={criteria}&c={category}&q={query}'
+        with open(path.join(path.dirname(__file__), "types.json"), "rt") as types:
+            types = json.load(types)
+            self.category = types['category']
+            self.criteria = types['criteria']
 
-    def get(self, query, criteria=0, category='0_0'):
+    def get(self, query, criteria=0, category='0_0', autoparse=True):
+        """
+        Gets the table of torrents from query site. Returns an lxml Element object if autoparse is False, returns an array containing dictionaries that tells the info of a torrent if autoparse is True. 
+        """
         searchHTML = requests.get(
             self.siteQuery.format(criteria=str(criteria),
                                   category=str(category),
@@ -20,11 +29,14 @@ class NyaaReq():
         tableData = list()
         for row in tableRow:
             tableData.append(row.findall('td'))
-        return tableData
+        return tableData if autoparse==False else self.parse(tableData)
 
-    def parse(self, tableRow):
+    def parse(self, table):
+        """
+        Parses the table rows (<tr> elements) to get the contents of it. Returns a list containing dictionaries, where a single dictionary contains information for a single torrent. 
+        """
         content = list()
-        for tableData in tableRow:
+        for tableData in table:
             category = tableData[0].find("a").attrib['href'][4:]
             name = tableData[1].find("a").attrib['title']
             url = self.site + tableData[1].find("a").attrib['href']
@@ -35,12 +47,37 @@ class NyaaReq():
             seed = tableData[5].text
             leech = tableData[6].text
             downloads = tableData[7].text
-            content.append([
-                category, name, url, torrent, magnet, size, date, seed, leech,
-                downloads
-            ])
+            content.append({
+                    'name':name,
+                    'url':url,
+                    'category':self.translate(category),
+                    'torrent':torrent,
+                    'magnet':magnet,
+                    'size': size,
+                    'date':date,
+                    'seed':seed,
+                    'leech':leech,
+                    'downloads':downloads
+            })
         return content
 
+    def translate(self, string):
+        """
+        Translates nyaa's URL to readable strings
+        """
+        try:
+            return self.category[string]
+        except:
+            try:
+                return self.criteria[string]
+            except:
+                return None
 
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query", help="String to search for in nyaa.si")
+    parser.add_argument("--criteria", help="Criteria to search for", nargs='?', default="0")
+    parser.add_argument("--category", help="Category to search in", nargs='?', default="0_0")
+    args = parser.parse_args()
+    nyaa = NyaaReq()
+    print(nyaa.get(args.query, args.criteria, args.category), sort_dicts=False)
